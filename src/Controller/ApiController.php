8a993d8ce5176226044967ce243e5e073a17d96c
tana <gogo71889@gmail.com>
2018-11-19 23:30:58 +0900

    API共通機能定義クラス作成

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -1,55 +1,24 @@
 <?php
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link      https://cakephp.org CakePHP(tm) Project
- * @since     0.2.9
- * @license   https://opensource.org/licenses/mit-license.php MIT License
- */
+
 namespace App\Controller;
 
-use Cake\Controller\Controller;
-use Cake\Event\Event;
+use App\Controller\AppController;
 
-/**
- * Application Controller
- *
- * Add your application-wide methods in the class below, your controllers
- * will inherit them.
- *
- * @link https://book.cakephp.org/3.0/en/controllers.html#the-app-controller
- */
-class AppController extends Controller
+class ApiController extends AppController
 {
-
-    /**
-     * Initialization hook method.
-     *
-     * Use this method to add common initialization code like loading components.
-     *
-     * e.g. `$this->loadComponent('Security');`
-     *
-     * @return void
-     */
     public function initialize()
     {
         parent::initialize();
-
-        $this->loadComponent('RequestHandler', [
-            'enableBeforeRedirect' => false,
-        ]);
-        $this->loadComponent('Flash');
-
-        /*
-         * Enable the following component for recommended CakePHP security settings.
-         * see https://book.cakephp.org/3.0/en/controllers/components/security.html
-         */
-        //$this->loadComponent('Security');
+    }
+    
+    /**
+     * レスポンス共通処理 - 200
+     * @param  array  $data
+     * @param  string $message
+     * @return JSONレスポンス
+     */
+    protected function response200($data = '', $message = 'Success')
+    {
+        return $this->response->withStringBody(json_encode(['message' => $message, 'data' => $data]));
     }
 }
