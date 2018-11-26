38f339d44c39efdb415ceb1c662be0b49604c647
tana <gogo71889@gmail.com>
2018-11-26 21:59:24 +0900

    QualitiesModelテストクラス実装

---
+++ tests/TestCase/Model/Master/QualitiesModelTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Test\TestCase\Model\Master;
+
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+use App\Model\Master\QualitiesModel;
+
+/**
+ * QualitiesModelTestクラス
+ * 
+ * [実行コマンド]
+ * vendor/bin/phpunit tests/TestCase/Model/Master/QualitiesModelTest.php
+ */
+class QualitiesModelTest extends TestCase {
+    
+    private $qualitiesModel;
+    private $qualities;
+    
+    public function setup()
+    {
+        parent::setUp();
+        $this->qualities = TableRegistry::get('Qualities');
+        $this->qualitiesModel = new QualitiesModel();
+    }
+    
+    /**
+     * ポケモン図鑑一覧取得
+     */
+    public function testgetList()
+    {
+        // 正常系パターン
+        $qualities = $this->qualitiesModel->getList()[0];
+        $this->assertEquals("1", $qualities['qualityId']);
+        $this->assertEquals("あくしゅう", $qualities['qualityName']);
+    }
+    
+    /**
+     * ポケモン図鑑取得
+     */
+    public function testget()
+    {
+        // NULLパターン
+        $this->assertEquals(null, $this->qualitiesModel->get(9999));
+        
+        // 正常系パターン
+        $quality = $this->qualitiesModel->get(1);
+        $this->assertEquals("1", $quality['qualityId']);
+        $this->assertEquals("あくしゅう", $quality['qualityName']);
+        $this->assertEquals("野生ポケモンと出会いにくくなる。攻撃した相手がたまにひるむ。", $quality['effect']);
+    }
+}
