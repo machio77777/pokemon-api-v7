5a9f276f6f2893d8161ea7cb756a487cd648b417
tana <gogo71889@gmail.com>
2018-11-26 21:51:35 +0900

    PokemonsModelテストクラス実装

---
+++ tests/TestCase/Model/Master/PokemonsModelTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Test\TestCase\Model\Master;
+
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+use App\Model\Master\PokemonsModel;
+
+/**
+ * PokemonsModelクラス
+ * 
+ * [実行コマンド]
+ * vendor/bin/phpunit tests/TestCase/Model/Master/PokemonsModelTest.php
+ */
+class PokemonsModelTest extends TestCase {
+    
+    private $pokemonsModel;
+    private $pokemons;
+    
+    public function setup()
+    {
+        parent::setUp();
+        $this->pokemons = TableRegistry::get('Pokemons');
+        $this->pokemonsModel = new PokemonsModel();
+    }
+    
+    /**
+     * ポケモン図鑑一覧取得
+     */
+    public function testgetList()
+    {
+        // 正常系パターン
+        $pokemon = $this->pokemonsModel->getList()[0];
+        $this->assertEquals("1", $pokemon['zukanNo']);
+        $this->assertEquals("1", $pokemon['subNo']);
+        $this->assertEquals("フシギダネ", $pokemon['name']);
+    }
+    
+    /**
+     * ポケモン図鑑取得
+     */
+    public function testget()
+    {
+        // NULLパターン
+        $this->assertEquals(null, $this->pokemonsModel->get(1, 100));
+        
+        // 正常系パターン
+        $pokemon = $this->pokemonsModel->get(1, 1);
+        $this->assertEquals("1", $pokemon['zukanNo']);
+        $this->assertEquals("1", $pokemon['subNo']);
+        $this->assertEquals("フシギダネ", $pokemon['name']);
+        $this->assertEquals("くさ", $pokemon['type1']);
+        $this->assertEquals("どく", $pokemon['type2']);
+        $this->assertEquals("しんりょく", $pokemon['quality1']);
+        $this->assertEquals(null, $pokemon['quality2']);
+        $this->assertEquals("ようりょくそ", $pokemon['dreamQuality']);
+        $this->assertEquals("45", $pokemon['hp']);
+        $this->assertEquals("49", $pokemon['at']);
+        $this->assertEquals("49", $pokemon['df']);
+        $this->assertEquals("65", $pokemon['sa']);
+        $this->assertEquals("65", $pokemon['sd']);
+        $this->assertEquals("45", $pokemon['sp']);
+    }
+}
