e56ba737f8666474fcdab96295968f38734d8af2
tana <gogo71889@gmail.com>
2018-12-02 17:50:15 +0900

    Tableクラス新規作成

---
+++ src/Model/Table/CompatibilitiesTable.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Pokemons Model
+ */
+class CompatibilitiesTable extends Table
+{
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('compatibilities');
+        $this->setPrimaryKey('id');
+    }
+}
---
+++ src/Model/Table/RoletargetsTable.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Pokemons Model
+ */
+class RoletargetsTable extends Table
+{
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('roletargets');
+        $this->setPrimaryKey('id');
+    }
+}
